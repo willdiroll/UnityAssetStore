59e7c4e43deefdc70915b586b2b80fef524a6199
William Diroll <diroll.9@osu.edu>
2022-01-23 18:57:19 -0500

    created a model for students, will add more after meeting to determine what models we need

--- UnityAssetStore/main/models.py
+++ UnityAssetStore/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.FirstName
