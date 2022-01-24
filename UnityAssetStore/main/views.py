8823e02949ef5370bd1401f95c546fbd9e4f288d
William Diroll <diroll.9@osu.edu>
2022-01-23 19:50:38 -0500

    changed index page to home page because its the home page not the index page obviously

--- UnityAssetStore/main/views.py
+++ UnityAssetStore/main/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Testing Text</h1>")
+def home(response):
+    return HttpResponse("<h1>Home Page</h1>")
