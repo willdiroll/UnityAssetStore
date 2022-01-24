fdfea4f5364e36039581f53a71b8e183ef058461
William Diroll <diroll.9@osu.edu>
2022-01-24 17:56:43 -0500

    created foundation for web pages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UnityAssetStore
 A project through CSE 5915 at The Ohio State University.
+
+
+Important to remember to turn off debug in settings.py once site is in production, that shouldn't be for a while.
--- UnityAssetStore/main/views.py
+++ UnityAssetStore/main/views.py
@@ -4,5 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(response):
-    return HttpResponse("<h1>Home Page</h1>")
+    return HttpResponse("<h1>Home/Assets Page</h1>")
 
+def join(response):
+    return HttpResponse("<h1>Join Page</h1>")
+
+def create(response):
+    return HttpResponse("<h1>Create Page</h1>")
+
+def asset_info(response):
+    return HttpResponse("<h1>Asset Info Page</h1>")
+
+def edit_asset(response):
+    return HttpResponse("<h1>Edit Asset Page</h1>")
+
+def add_asset(response):
+    return HttpResponse("<h1>Add Asset Page</h1>")
+
+def credits(response):
+    return HttpResponse("<h1>Credits Page</h1>")
