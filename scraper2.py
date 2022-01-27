ea956af7d5456e21f6053bcebcc84144dbb0ad69
cepedad <cepedadmc@gmail.com>
2022-01-27 17:20:45 -0500

    Did more work on scraper using Selenium for scraping Asset links.

---
+++ scraper2.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# User info
+EMAIL = "lainey.chylik@gmail.com"
+PASSWORD = "Password22"
+
+# Set up a headless Firefox browser
+op = webdriver.FirefoxOptions()	# Must download the Firefox geckodriver from
+								# https://github.com/mozilla/geckodriver/releases
+								# Then, move geckodriver into a $PATH directory
+								#			e.g. usr/local/bin
+op.headless = False		# Can change this to True to avoid opening FireFox window
+br = webdriver.Firefox(options = op)
+
+# Navigate to the unity Asset Store login page
+URL = 'https://assetstore.unity.com/account/assets'
+br.get(URL)
+
+# Locate the login fields
+email_field = br.find_element(By.ID, 'conversations_create_session_form_email')
+password_field = br.find_element(By.ID, 'conversations_create_session_form_password')
+login_button = br.find_element(By.NAME, 'commit')
+
+# Log In
+email_field.send_keys(EMAIL)
+password_field.send_keys(PASSWORD)
+login_button.click()
+
+""" WIP
+# Change asset page to show 100 assets
+br.find_elements(By.CLASS_NAME, 'Y1yB6')[1].click()
+page_size = br.find_elements(By.CLASS_NAME, '_3BlIq')[6]
+try:
+	page_size.click()
+except:
+	br.implicitly_wait(2)
+	page_size.click()
+
+br.implicitly_wait(10)
+"""
+
+# Gather meta-data for each asset on the page
+links = []
+assets = br.find_elements(By.CLASS_NAME, '_161YN')
+for asset in assets:
+	br.execute_script('arguments[0].scrollIntoView();', asset)
+	try:
+		asset.click()
+	except:
+		br.implicitly_wait(5)
+		asset.click()
+	links.append(br.find_element(By.CLASS_NAME, '_3UE3J.ZQFsR.auto._2RWe1').get_attribute('href'))
+	br.find_element(By.CLASS_NAME, '_1VOoF').click()
+
+# Close the browser
+br.quit()
+
+# Print list of links
+for link in links:
+	print(link)
+
+print(len(links))
--- scraper3.py
+++ scraper3.py
@@ -1,11 +1,28 @@
-from requests_html import HTML
+import requests_html
+import requests
 import mechanize
 from bs4 import BeautifulSoup
 
 # User and webpage data
 URL = 'https://assetstore.unity.com/account/assets'
+LOGIN_URL = 'https://id.unity.com/en/conversations/1fec57c6-ea1e-423d-960a-25d837306360012f'
 EMAIL = 'lainey.chylik@gmail.com'
 PASSWORD = 'Password22'
+payload = {
+	'utf8' : '✓',
+	'_method' : 'put',
+	'authenticity_token' : 'mh3k3btVczByEZwNtkLYYrJRLosdFUy9ey1+XWTmPO+So/tRP77mXWLlyocBHG1zQ/ikatPMStMkLKj3oZmVtQ==',
+	'conversations_create_session_form[email]' : EMAIL,
+	'conversations_create_session_form[password]' : PASSWORD,
+	'conversations_create_session_form[remember_me]' : 'false',
+	'commit' : 'Sign in'
+}
+
+"""
+with requests_html.HTMLSession() as session:
+	post = session.post(LOGIN_URL, data=payload)
+	print(post.text)
+"""
 
 # Set up a mechanize Browser object (for submitting login form)
 br = mechanize.Browser()
@@ -24,8 +41,13 @@ br.submit()
 
 # Redirect to the asset store
 asset_store = br.follow_link( br.links()[0])
+with requests_html.HTMLSession() as session:
+	print(dir(br.links()[0]))
 
+"""
 # Start a Request-HTML session
 raw_html = BeautifulSoup(asset_store, 'html.parser')
 session = HTML(html = raw_html.prettify())
-session.render()
+for i in session.links:
+	print(i)
+"""
