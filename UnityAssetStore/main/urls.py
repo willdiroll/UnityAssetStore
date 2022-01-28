fb02d1380633023ce43f4d631123e31252f26627
TylerCingel <tccingel@gmail.com>
2022-01-27 23:58:42 -0500

    Page updates
    Added some of the content to the following pages: join, create, credits, home. Also added path for home page.

--- UnityAssetStore/main/urls.py
+++ UnityAssetStore/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("join/", views.join, name="join"),
     path("create/", views.create, name="create"),
     path("asset_info/", views.asset_info, name="asset_info"),
