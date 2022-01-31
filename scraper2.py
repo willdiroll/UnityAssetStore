9c796e41158da4370236fe5e63e02483282c1c41
cepedad <cepedadmc@gmail.com>
2022-01-31 16:31:13 -0500

    Added pagination and ability to expand item limit to 100 assets per page.

--- scraper2.py
+++ scraper2.py
@@ -3,18 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
+
 # User info
 EMAIL = "lainey.chylik@gmail.com"
 PASSWORD = "Password22"
 
 # Set up a headless Firefox browser
 op = webdriver.FirefoxOptions()	# Must download the Firefox geckodriver from
-					# https://github.com/mozilla/geckodriver/releases
-					# Then, move geckodriver into a $PATH directory
-					#			e.g. usr/local/bin
+								# https://github.com/mozilla/geckodriver/releases
+								# Then, move geckodriver into a $PATH directory
+								#			e.g. usr/local/bin
 op.headless = False		# Can change this to True to avoid opening FireFox window
 br = webdriver.Firefox(options = op)
-wait = WebDriverWait(br, 2)
+wait = WebDriverWait(br, 5)
 
 # Navigate to the unity Asset Store login page
 URL = 'https://assetstore.unity.com/account/assets'
@@ -31,28 +33,34 @@ email_field.send_keys(EMAIL)
 password_field.send_keys(PASSWORD)
 login_button.click()
 
-""" WIP
-# Change asset page to show 100 assets
+# Entered the 'My Asset Page'
+# Show 100 assets per page
 br.find_elements(By.CLASS_NAME, 'Y1yB6')[1].click()
 page_size = br.find_elements(By.CLASS_NAME, '_3BlIq')[6]
-try:
-	page_size.click()
-except:
-	br.implicitly_wait(2)
-	page_size.click()
+wait.until(EC.element_to_be_clickable(page_size)).click()
+time.sleep(2)
 
-br.implicitly_wait(10)
-"""
-
-# Gather meta-data for each asset on the page
+# Loop thru Asset pages
+has_next_page = True
 links = []
-assets = br.find_elements(By.CLASS_NAME, '_161YN')
-for asset in assets:
-	br.execute_script('arguments[0].scrollIntoView();', asset)
+while has_next_page:
 
-	wait.until(EC.element_to_be_clickable(asset)).click()
-	links.append(br.find_element(By.CLASS_NAME, '_3UE3J.ZQFsR.auto._2RWe1').get_attribute('href'))
-	br.find_element(By.CLASS_NAME, '_1VOoF').click()
+	# Loop thru assets on the current page
+	assets = br.find_elements(By.CLASS_NAME, '_161YN')
+	for asset in assets:
+		br.execute_script('arguments[0].scrollIntoView();', asset)
+
+		wait.until(EC.element_to_be_clickable(asset)).click()
+		links.append(br.find_element(By.CLASS_NAME, '_3UE3J.ZQFsR.auto._2RWe1').get_attribute('href'))
+		br.find_element(By.CLASS_NAME, '_1VOoF').click()
+
+	# Check for next page
+	next_page = br.find_element(By.CLASS_NAME, '_3UE3J.auto._3wJlw.F35ws')
+	has_next_page = next_page.is_enabled()
+
+	# Click next page button if it is enabled
+	if has_next_page:
+		wait.until(EC.element_to_be_clickable(next_page)).click()
 
 # Close the browser
 br.quit()
@@ -62,3 +70,5 @@ for link in links:
 	print(link)
 
 print(len(links))
+
+
