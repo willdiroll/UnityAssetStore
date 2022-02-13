0cc46feb7de8d9e6728e05ed9eca1ac56bdc91c1
William Diroll <diroll.9@osu.edu>
2022-02-13 18:50:09 -0500

    Database is done using the knowledge we know now

--- UnityAssetStore/main/models.py
+++ UnityAssetStore/main/models.py
@@ -1,25 +1,26 @@
+from distutils.archive_util import make_zipfile
 from django.db import models
 
 # Create your models here.
+#
+#class student(models.Model):
+#    FirstName = models.CharField(max_length=100)
+#    LastName = models.CharField(max_length=100)
+#    Email = models.CharField(max_length=100)
+#
+#    def __str__(self):
+#        return self.FirstName
 
-class student(models.Model):
-    FirstName = models.CharField(max_length=100)
-    LastName = models.CharField(max_length=100)
-    Email = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.FirstName
-
-class student_class(models.Model):
-    Email = models.CharField(max_length=100) #add FK to students
-    AID = models.CharField(max_length=100) #add FK to asset_info
-
-    def __str__(self):
-        return self.Email
+#class student_class(models.Model):
+#    Email = models.CharField(max_length=100) #add FK to students
+#    AID = models.CharField(max_length=100) #add FK to asset_info
+#
+#    def __str__(self):
+#        return self.Email
 
 
-class asset_info(models.Model):
-    AID = models.CharField(max_length=100)
+class Asset(models.Model):
+    AID = models.IntegerField(primary_key=True)
     AssetName = models.CharField(max_length=100)
     AssetLink = models.CharField(max_length=100)
     Description = models.CharField(max_length=100)
@@ -31,26 +32,32 @@ class asset_info(models.Model):
     def __str__(self):
         return self.AssetName
 
-class professor(models.Model):
-    ProfFirstName = models.CharField(max_length=100)
-    ProfLastName = models.CharField(max_length=100)
-    ProfEmail = models.CharField(max_length=100)
+class Repo(models.Model):
+    RepoKey = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Identifier = models.CharField(max_length=100)
+    AID = models.ForeignKey(Asset, default=None, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.ProfFirstName
+#class professor(models.Model):
+#    ProfFirstName = models.CharField(max_length=100)
+#    ProfLastName = models.CharField(max_length=100)
+#    ProfEmail = models.CharField(max_length=100)
+#
+#    def __str__(self):
+#        return self.ProfFirstName
 
-class class_info(models.Model):
-    ClassKey = models.CharField(max_length=100)
-    ProfEmail = models.CharField(max_length=100) #Add FK back to professors model
-    ClassName = models.CharField(max_length=100)
+#class class_info(models.Model):
+#    ClassKey = models.CharField(max_length=100)
+#    ProfEmail = models.CharField(max_length=100) #Add FK back to professors model
+#    ClassName = models.CharField(max_length=100)
+#
+#    def __str__(self):
+#        return self.ClassName
 
-    def __str__(self):
-        return self.ClassName
+#class class_asset(models.Model):
+#   ClassKey = models.CharField(max_length=100) #add FK to class_info
+#    AID = models.CharField(max_length=100) #add FK to asset_info
 
-class class_asset(models.Model):
-    ClassKey = models.CharField(max_length=100) #add FK to class_info
-    AID = models.CharField(max_length=100) #add FK to asset_info
-
-    def __str__(self):
-        return self.ClassKey
+#    def __str__(self):
+#        return self.ClassKey
 
