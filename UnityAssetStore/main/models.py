2092e0edce065b3de1827a241a4c32af77fb7521
William Diroll <diroll.9@osu.edu>
2022-02-13 19:45:21 -0500

    Removed Unnecessary Comments on DB

--- UnityAssetStore/main/models.py
+++ UnityAssetStore/main/models.py
@@ -1,24 +1,6 @@
 from distutils.archive_util import make_zipfile
 from django.db import models
 
-# Create your models here.
-#
-#class student(models.Model):
-#    FirstName = models.CharField(max_length=100)
-#    LastName = models.CharField(max_length=100)
-#    Email = models.CharField(max_length=100)
-#
-#    def __str__(self):
-#        return self.FirstName
-
-#class student_class(models.Model):
-#    Email = models.CharField(max_length=100) #add FK to students
-#    AID = models.CharField(max_length=100) #add FK to asset_info
-#
-#    def __str__(self):
-#        return self.Email
-
-
 class Asset(models.Model):
     AID = models.IntegerField(primary_key=True)
     AssetName = models.CharField(max_length=100)
@@ -37,27 +19,3 @@ class Repo(models.Model):
     Name = models.CharField(max_length=100)
     Identifier = models.CharField(max_length=100)
     AID = models.ForeignKey(Asset, default=None, on_delete=models.CASCADE)
-
-#class professor(models.Model):
-#    ProfFirstName = models.CharField(max_length=100)
-#    ProfLastName = models.CharField(max_length=100)
-#    ProfEmail = models.CharField(max_length=100)
-#
-#    def __str__(self):
-#        return self.ProfFirstName
-
-#class class_info(models.Model):
-#    ClassKey = models.CharField(max_length=100)
-#    ProfEmail = models.CharField(max_length=100) #Add FK back to professors model
-#    ClassName = models.CharField(max_length=100)
-#
-#    def __str__(self):
-#        return self.ClassName
-
-#class class_asset(models.Model):
-#   ClassKey = models.CharField(max_length=100) #add FK to class_info
-#    AID = models.CharField(max_length=100) #add FK to asset_info
-
-#    def __str__(self):
-#        return self.ClassKey
-
