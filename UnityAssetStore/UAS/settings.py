cef17eddab410e9aa8e7cd1163bffe6cbaa9f43f
Lainey Chylik <laineychylik@Laineys-MacBook-Pro.local>
2022-03-20 15:11:28 -0400

    trying to get postgres to work

--- UnityAssetStore/UAS/settings.py
+++ UnityAssetStore/UAS/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'UAS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uas',
+        'USER': 'lainey',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
