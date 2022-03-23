43dd27761da5cd3fc7f802c8bbc31f78729822c0
Lainey Chylik <laineychylik@Laineys-MBP.lan1>
2022-03-22 20:10:24 -0400

    added functionality for postgres db, connecting to postgres db in settings.py. updated the scraper.py to work with postgres as well. created the postgres db too.

--- UnityAssetStore/UAS/settings.py
+++ UnityAssetStore/UAS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'django.contrib.sites',
     'main.apps.MainConfig',
 ]
 
@@ -76,12 +77,12 @@ WSGI_APPLICATION = 'UAS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'uas',
         'USER': 'lainey',
         'PASSWORD': 'test',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
