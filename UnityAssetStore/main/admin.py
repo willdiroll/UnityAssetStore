aade1787e1e7e633a4212ac3f7cc105bfbfb06cc
Diego Cepeda <cepedad@users.noreply.github.com>
2022-04-18 15:49:59 -0400

    Delete UnityAssetStore directory

--- UnityAssetStore/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Repo, Asset, Label, Category
-# Register your models here.
-admin.site.register(Repo)
-admin.site.register(Asset)
-admin.site.register(Label)
-admin.site.register(Category)
